7f12ea2593034fbca58c97500eb638ee8db9131d
DungNguyen <dungnguyentien0409@gmail.com>
2020-06-29 10:16:39 +0800

    update top-bar

--- src/app/core/layout/navigation/navigation.component.ts
+++ src/app/core/layout/navigation/navigation.component.ts
@@ -6,10 +6,6 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent implements OnInit {
-    isSideBarExpand: boolean = false;
     ngOnInit() {}
 
-    onClickBackDrop() {
-
-    }
 }
